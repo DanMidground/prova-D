052cb3661350dbae910e7db67d739b79840177d9
Daniel <0001226796@senaimgaluno.com.br>
2026-07-02 15:58:11 -0300

    Refatoração : Adicionado método de verificação de saúde

--- InimigoIA.cs
+++ InimigoIA.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class InimigoIA
+{
+public float Velocidade { get; set; } = 2.0f;
+public float PosicaoX { get; private set; } = 0.0f;
+
+public void Mover(float tempo)
+{
+PosicaoX += Velocidade * tempo;
+Console.WriteLine($"Inimigo se moveu para a posição: {PosicaoX}");
+}
+}
+
+public void VerificarSaude()
+{
+if (Vida <= 0)
+{
+Console.WriteLine("Inimigo derrotado!");
+}
+else
+{
+Console.WriteLine($"Inimigo ativo com {Vida}
+de vida.");
+}
+}
